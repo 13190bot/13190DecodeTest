21fbf6de3ef695c32334d80ce258b6e497a72605
Shinya Tsukamoto <shinyantsukamoto@gmail.com>
2025-11-23 18:33:24 -0800

    planned paths on meepmeep (may change to splines later)

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -6,6 +6,7 @@ import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
+
 public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(600);
@@ -15,7 +16,15 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(30, 0, 0))
+        int shoot = 2;
+        int stop = 1;
+        int x =0; //shooting place
+        int y =0; //shooting place
+//ADD A 30 SEC TIMER ON A WHILE LOOP FOR AUTOOP
+
+        myBot.runAction(
+                myBot.getDrive()
+                        .actionBuilder(new Pose2d(62, 35, Math.toRadians(180)))
 
 
 
@@ -34,14 +43,64 @@ public class MeepMeepTesting {
 
 
 
+//Red far from goal, head is inttake back is outtake
+//only 2 balls can fit in the robot
+
+                        .waitSeconds(3)
+
+                .strafeToLinearHeading(new Vector2d(37, 30), Math.toRadians(90))
+                .waitSeconds(stop)
+                .lineToY(37)
+                .strafeToLinearHeading(new Vector2d(x, y), Math.toRadians(135))
+                .waitSeconds(shoot)
 
 
 
 
-                .splineToLinearHeading(new Pose2d(40, 40, Math.toRadians(90)), Math.toRadians(0))
+                .strafeToLinearHeading(new Vector2d(37, 30), Math.toRadians(90))
+                .waitSeconds(stop)
+                .lineToY(40)
+                .strafeToLinearHeading(new Vector2d(37, 60), Math.toRadians(0))
+                .waitSeconds(stop)
+                .lineToX(50)
+                .strafeToLinearHeading(new Vector2d(x, y), Math.toRadians(135))
+                .waitSeconds(shoot)
 
+                .strafeToLinearHeading(new Vector2d(37, 30), Math.toRadians(90))
+                .waitSeconds(stop)
+                .lineToY(40)
+                .strafeToLinearHeading(new Vector2d(37, 60), Math.toRadians(0))
+                .waitSeconds(stop)
+                .lineToX(62)
+                .strafeToLinearHeading(new Vector2d(x, y), Math.toRadians(135))
+                .waitSeconds(shoot)
 
-                // .turn(Math.toRadians(90))
+                .strafeToLinearHeading(new Vector2d(15, 30), Math.toRadians(90))
+                .waitSeconds(stop)
+                .lineToY(40)
+                .strafeToLinearHeading(new Vector2d(x, y), Math.toRadians(135))
+                .waitSeconds(shoot)
+
+                .strafeToLinearHeading(new Vector2d(15, 30), Math.toRadians(90))
+                .waitSeconds(stop)
+                .lineToY(50)
+                .strafeToLinearHeading(new Vector2d(-15, 10), Math.toRadians(90))
+                .waitSeconds(stop)
+                .lineToY(40)
+                .strafeToLinearHeading(new Vector2d(x, y), Math.toRadians(135))
+                .waitSeconds(shoot)
+
+                .strafeToLinearHeading(new Vector2d(-15, 30), Math.toRadians(90))
+                .waitSeconds(stop)
+                .lineToY(50)
+                .strafeToLinearHeading(new Vector2d(x, y), Math.toRadians(135))
+                .waitSeconds(shoot)
+
+                .turnTo(Math.toRadians(90))
+                .waitSeconds(stop)
+                .lineToY(50)
+
+//
 
 
 
