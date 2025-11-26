a7e16cd0a86a176e7e17d71e2c8f9b6dbb9b9b02
Shinya Tsukamoto <shinyantsukamoto@gmail.com>
2025-11-25 22:39:02 -0800

    made spline mapping for red and blue side

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTestingSpline2reflected.java
@@ -0,0 +1,149 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+
+public class MeepMeepTestingSpline2reflected {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(600);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        int shoot = 2;
+        int stop = 1;
+        int x =1; //shooting place
+        int y =1; //shooting place
+//ADD A 30 SEC TIMER ON A WHILE LOOP FOR AUTOOP
+
+        myBot.runAction(
+                myBot.getDrive()
+                        .actionBuilder(new Pose2d(-52, -50, Math.toRadians(45)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Red close to goal, head is inttake back is outtake
+//only 2 balls can fit in the robot
+//                        add a 3,5, 10 sec delay
+
+                        .waitSeconds(3)
+
+
+
+                        .splineTo(new Vector2d(-15, -10), Math.toRadians(270))
+                        .waitSeconds(stop)
+                        //run intake
+                        .lineToY(-35)
+                        .splineTo(new Vector2d(x, -y), Math.toRadians(215))
+                        .waitSeconds(shoot)
+
+                        .splineTo(new Vector2d(-15, -10), Math.toRadians(270))
+                        .waitSeconds(stop)
+                        // run intake
+                        .lineToY(-50)
+                        .splineTo(new Vector2d(15, -15), Math.toRadians(270))
+                        .waitSeconds(stop)
+                        .lineToY(-30)
+                        .splineToLinearHeading(new Pose2d(x, -y, Math.toRadians(215)), Math.toRadians(0))
+                        .waitSeconds(shoot)
+
+                        .splineTo(new Vector2d(15, -15), Math.toRadians(270))
+                        .waitSeconds(stop)
+                        //run intake
+                        .lineToY(-50)
+                        .splineToLinearHeading(new Pose2d(x, -y, Math.toRadians(215)), Math.toRadians(90))
+                        .waitSeconds(shoot)
+
+                        .splineTo(new Vector2d(37, -30), Math.toRadians(270))
+                        .waitSeconds(stop)
+                        //run intake
+                        .lineToY(-32)
+                        .splineTo(new Vector2d(x, -y), Math.toRadians(215))
+                        .waitSeconds(shoot)
+
+                        .splineTo(new Vector2d(37, -30), Math.toRadians(270))
+                        .waitSeconds(stop)
+                        //run intake
+                        .lineToY(-40)
+                        .splineTo(new Vector2d(37, -60), Math.toRadians(0))
+                        .waitSeconds(stop)
+                        .lineToX(45)
+                        .splineTo(new Vector2d(x, -y), Math.toRadians(215))
+                        .waitSeconds(shoot)
+
+                        .splineTo(new Vector2d(37, -30), Math.toRadians(270))
+                        .waitSeconds(stop)
+                        .lineToY(-40)
+                        .splineTo(new Vector2d(37, -60), Math.toRadians(0))
+                        .waitSeconds(stop)
+                        //run intake
+                        .lineToX(55)
+                        .splineTo(new Vector2d(x, -y), Math.toRadians(215))
+                        .waitSeconds(shoot)
+
+                        .turnTo(Math.toRadians(270))
+                        .waitSeconds(stop)
+                        .lineToY(-50)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                        .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_BLACK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
