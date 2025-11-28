42f6ddd0efa70698c2c4db8af4b048689cd5c3ee
Shinya Tsukamoto <shinyantsukamoto@gmail.com>
2025-11-28 14:45:52 -0800

    added some methods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SigmaAutoOpWithoutRR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SigmaAutoOpWithoutRR.java
@@ -18,6 +18,8 @@ public class SigmaAutoOpWithoutRR extends LinearOpMode {
     DcMotor outtakeMotor;
     Servo platform;
 
+    private ElapsedTime runtime = new ElapsedTime();
+
     @Override
     public void runOpMode() {
         // Initialize hardware
@@ -28,7 +30,7 @@ public class SigmaAutoOpWithoutRR extends LinearOpMode {
         intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
         outtakeMotor = hardwareMap.get(DcMotor.class, "outtakeMotor");
         platform = hardwareMap.get(Servo.class, "platform");
-        platform.scaleRange(0,1);
+        platform.scaleRange(0, 1);
 
 
         // Set motor directions
@@ -38,19 +40,135 @@ public class SigmaAutoOpWithoutRR extends LinearOpMode {
         frontLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
 
 
-
         if (isStopRequested()) return;
 
         // Wait for start
         waitForStart();
+        runtime.reset();
 
 
         // === Autonomous Sequence ===
-        drive(0.5, 2000);
+        int stop = 500;
+        int x = 1; //shooting
+        int y = 1; //shooting
 
-        // Stop motors
+
+        forward(25);
+        sleep(stop);
+        turn(90);
+        sleep(stop);
+        intakeMotor.setPower(1);
+        forward(30);
+        sleep(stop);
+        backward(50 - y);
+        sleep(stop);
+        turncc(90);
+        sleep(stop);
+        forward(37 - x);
+        sleep(stop);
+        turn(45);
+        sleep(stop);
+        shoot(3);
+        sleep(stop);
+        turn(135);
+        sleep(stop);
+        forward(13-x);
+        sleep(stop);
+        turncc(90);
+        sleep(stop);
+        forward(50-y);
+        sleep(stop);
+        backward(50 - y);
+        sleep(stop);
+        turncc(90);
+        sleep(stop);
+        forward(37 - x);
+        sleep(stop);
+        turn(45);
+        sleep(stop);
+        shoot(3);
+
+
+        //make everything stop when runtime is > 30 seconds
+stopAll();
+
+
+    }
+
+    // Helper to stop all drive motors
+    private void stopDrive() {
+            frontLeftMotor.setPower(0);
+            backLeftMotor.setPower(0);
+            frontRightMotor.setPower(0);
+            backRightMotor.setPower(0);
+
+        }
+
+    // Helper to stop all drive motors
+    private void stopAll() {
+        frontLeftMotor.setPower(0);
+        backLeftMotor.setPower(0);
+        frontRightMotor.setPower(0);
+        backRightMotor.setPower(0);
+        outtakeMotor.setPower(0);
+        intakeMotor.setPower(0);
+
+    }
+
+        // Helper method to turn clockwise
+    //assuming it takes 4 seconds for 360 degrees
+    private void turn(int degrees) {
+        frontLeftMotor.setPower(0.5);
+        backLeftMotor.setPower(0.5);
+        frontRightMotor.setPower(-0.5);
+        backRightMotor.setPower(-0.5);
+        sleep((int)(degrees *(1000/90)));  // 360 degrees divided by 4 seconds = 90
         stopDrive();
     }
+
+    // Helper method to turn counterclockwise
+    //assuming it takes 4 seconds for 360 degrees
+    private void turncc(int degrees) {
+        frontLeftMotor.setPower(-0.5);
+        backLeftMotor.setPower(-0.5);
+        frontRightMotor.setPower(0.5);
+        backRightMotor.setPower(0.5);
+        sleep((int)(degrees *(1000/90)));  // 360 degrees divided by 4 seconds = 90
+            stopDrive();
+    }
+
+
+    // Helper method to drive forward
+    //assuming 1 unit in meepmeep is
+    private void forward(int unit) {
+        frontLeftMotor.setPower(0.5);
+        backLeftMotor.setPower(0.5);
+        frontRightMotor.setPower(0.5);
+        backRightMotor.setPower(0.5);
+        sleep((int)(unit * (1000/5)));
+            stopDrive();
+    }
+
+    private void backward(int unit) {
+        frontLeftMotor.setPower(-0.5);
+        backLeftMotor.setPower(-0.5);
+        frontRightMotor.setPower(-0.5);
+        backRightMotor.setPower(-0.5);
+        sleep((int)(unit * (1000/5)));
+            stopDrive();
+    }
+
+    private void shoot(int balls) {
+        outtakeMotor.setPower(1);
+
+      for (; balls>0; balls--) {
+          platform.setPosition(1);
+          sleep(1000);
+          platform.setPosition(0);
+          sleep(1000);
+      }
+    }
+
 
     // Helper method to drive in one direction for time (ms)
     private void drive(double power, int timeMs) {
@@ -59,17 +177,11 @@ public class SigmaAutoOpWithoutRR extends LinearOpMode {
         frontRightMotor.setPower(power);
         backRightMotor.setPower(power);
         sleep(timeMs);
+            stopDrive();
     }
 
-    // Helper to stop all driv e motors
-    private void stopDrive() {
-        frontLeftMotor.setPower(0);
-        backLeftMotor.setPower(0);
-        frontRightMotor.setPower(0);
-        backRightMotor.setPower(0);
+
+//adb connect 192.168.43.1:5555
 
 
-
-
-    }
 }
