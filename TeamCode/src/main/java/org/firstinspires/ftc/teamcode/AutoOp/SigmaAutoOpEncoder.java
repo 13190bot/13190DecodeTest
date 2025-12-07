16a822fc1f5fe2ddc28aaf4cf10f772cd7e98003
Shinya Tsukamoto <shinyantsukamoto@gmail.com>
2025-12-07 10:18:48 -0800

    made folders to clean up

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOp/SigmaAutoOpEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOp/SigmaAutoOpEncoder.java
@@ -1,6 +1,6 @@
 // may work more consistent due to battery stuff with time based auto
 
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.AutoOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
