2d22c9f1726b8859ade82fc53295bf26fa1f4ed1
Shinya Tsukamoto <shinyantsukamoto@gmail.com>
2025-12-24 14:25:13 -0800

    fixed merge conflict

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOps/IntakeOuttakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOps/IntakeOuttakeTest.java
@@ -1,231 +1,92 @@
 package org.firstinspires.ftc.teamcode.TestOps;
 
-<<<<<<< HEAD
+
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-
-=======
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
->>>>>>> a0bbf905b93dd7c8326ba25365218c30e5f026ac
 import com.arcrobotics.ftclib.gamepad.*;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
-<<<<<<< HEAD
 
 import org.firstinspires.ftc.teamcode.Utils.Subsystem.*;
 
 
 
-
-=======
-import org.firstinspires.ftc.teamcode.Utils.Subsystem.*;
-
-
->>>>>>> a0bbf905b93dd7c8326ba25365218c30e5f026ac
 @TeleOp
 public class IntakeOuttakeTest extends LinearOpMode {
 
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> a0bbf905b93dd7c8326ba25365218c30e5f026ac
     DcMotor intakeMotor;
     DcMotor outtakeMotor;
 
 
 
 
-<<<<<<< HEAD
-
-
-
 
     @Override
     public void runOpMode() throws InterruptedException {
 
 
-=======
-    @Override
-    public void runOpMode() throws InterruptedException {
-
->>>>>>> a0bbf905b93dd7c8326ba25365218c30e5f026ac
         intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
         outtakeMotor = hardwareMap.get(DcMotor.class, "outtakeMotor");
 
 
 
 
-<<<<<<< HEAD
-
-
-
 
         waitForStart();
-
 
         if (isStopRequested()) return;
 
 
-=======
-        waitForStart();
-
-        if (isStopRequested()) return;
-
->>>>>>> a0bbf905b93dd7c8326ba25365218c30e5f026ac
         while (opModeIsActive() && !isStopRequested()) {
 
 
 
 
-
-<<<<<<< HEAD
-
-
-
-
-
-=======
->>>>>>> a0bbf905b93dd7c8326ba25365218c30e5f026ac
             if (gamepad1.triangle){
                 intakeMotor.setPower(0.7);
             }
 
-<<<<<<< HEAD
 
-=======
->>>>>>> a0bbf905b93dd7c8326ba25365218c30e5f026ac
             if (gamepad1.left_bumper) {
                 outtakeMotor.setPower(1);
             }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> a0bbf905b93dd7c8326ba25365218c30e5f026ac
             if (gamepad1.right_bumper) {
                 outtakeMotor.setPower(0);
             }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> a0bbf905b93dd7c8326ba25365218c30e5f026ac
             if (gamepad1.dpad_up) {
                 outtakeMotor.setPower(outtakeMotor.getPower()+0.1);
             }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> a0bbf905b93dd7c8326ba25365218c30e5f026ac
             if (gamepad1.dpad_down) {
                 outtakeMotor.setPower(outtakeMotor.getPower()-0.1);
             }
 
 
-
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> a0bbf905b93dd7c8326ba25365218c30e5f026ac
             telemetry.addLine("left bumper: outtake = 1");
             telemetry.addLine("right bumper: outtake = 0");
             telemetry.addLine("dpad up: outtake power + 0.1");
             telemetry.addLine("dpad down: outtake power - 0.1");
             telemetry.addLine("triangle: intake = 0.7");
 
-<<<<<<< HEAD
+
 
             telemetry.addData("intake power:", intakeMotor.getPower());
             telemetry.addData("outtake power:", outtakeMotor.getPower());
 
 
-=======
-            telemetry.addData("intake power:", intakeMotor.getPower());
-            telemetry.addData("outtake power:", outtakeMotor.getPower());
-
->>>>>>> a0bbf905b93dd7c8326ba25365218c30e5f026ac
             telemetry.update();
 
 
 
 
-<<<<<<< HEAD
 
 
-
-
-=======
->>>>>>> a0bbf905b93dd7c8326ba25365218c30e5f026ac
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<<<<<<< HEAD
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=======
->>>>>>> a0bbf905b93dd7c8326ba25365218c30e5f026ac
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOps/ShootingTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOps/ShootingTest.java
@@ -1,21 +1,5 @@
 package org.firstinspires.ftc.teamcode.TestOps;
 
-<<<<<<< HEAD
-
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-
-import com.arcrobotics.ftclib.gamepad.*;
-import com.qualcomm.robotcore.hardware.Gamepad;
-
-
-import org.firstinspires.ftc.teamcode.Utils.Subsystem.*;
-
-
-
-
-=======
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -25,71 +9,32 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import org.firstinspires.ftc.teamcode.Utils.Subsystem.*;
 
 
->>>>>>> a0bbf905b93dd7c8326ba25365218c30e5f026ac
+
 @TeleOp
 public class ShootingTest extends LinearOpMode {
 
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> a0bbf905b93dd7c8326ba25365218c30e5f026ac
     private Shooting shooting;
     public static double platformPower = 1;
 
 
-<<<<<<< HEAD
-
-
     @Override
     public void runOpMode() throws InterruptedException {
 
-
-=======
-    @Override
-    public void runOpMode() throws InterruptedException {
-
->>>>>>> a0bbf905b93dd7c8326ba25365218c30e5f026ac
         shooting = new Shooting(hardwareMap);
 
 
-
-
-
-<<<<<<< HEAD
-
-
-
-
-
-        waitForStart();
-
-
-        if (isStopRequested()) return;
-
-
-=======
         waitForStart();
 
         if (isStopRequested()) return;
 
->>>>>>> a0bbf905b93dd7c8326ba25365218c30e5f026ac
         while (opModeIsActive() && !isStopRequested()) {
 
 
-
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> a0bbf905b93dd7c8326ba25365218c30e5f026ac
             if (gamepad1.left_bumper){
                 shooting.intakeMotor.setPower(0.7);
             }
 
-<<<<<<< HEAD
 
             if (gamepad1.right_bumper){
                 shooting.platformRight.setPosition(platformPower);
@@ -97,128 +42,31 @@ public class ShootingTest extends LinearOpMode {
             }
 
 
-=======
-            if (gamepad1.right_bumper){
-            shooting.platformRight.setPosition(platformPower);
-            shooting.platformRight.setPosition(platformPower);
-            }
-
->>>>>>> a0bbf905b93dd7c8326ba25365218c30e5f026ac
             if (gamepad1.dpad_up) {
                 shooting.outtakeMotor.setPower(shooting.outtakeMotor.getPower()+0.1);
             }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> a0bbf905b93dd7c8326ba25365218c30e5f026ac
             if (gamepad1.dpad_down) {
                 shooting.outtakeMotor.setPower(shooting.outtakeMotor.getPower()-0.1);
             }
 
-<<<<<<< HEAD
 
-=======
->>>>>>> a0bbf905b93dd7c8326ba25365218c30e5f026ac
             telemetry.addLine("Left Bumper: intake = 0.7");
             telemetry.addLine("Right Bumper: platform power smth");
             telemetry.addLine("dpad up: outtake + 0.1");
             telemetry.addLine("dpad up: outtake - 0.1");
 
-<<<<<<< HEAD
-
-=======
->>>>>>> a0bbf905b93dd7c8326ba25365218c30e5f026ac
             telemetry.addData("Platform Right", shooting.platformRight.getPosition());
             telemetry.addData("Platform Left", shooting.platformLeft.getPosition());
             telemetry.addData("Outtake", shooting.outtakeMotor.getPower());
             telemetry.addData("Intake", shooting.intakeMotor.getPower());
 
-<<<<<<< HEAD
-
-=======
->>>>>>> a0bbf905b93dd7c8326ba25365218c30e5f026ac
             telemetry.addData("Outtake encoder", shooting.outtakeMotor.getCurrentPosition());
             telemetry.addData("Intake encoder", shooting.intakeMotor.getCurrentPosition());
 
-
-<<<<<<< HEAD
-
-
-=======
->>>>>>> a0bbf905b93dd7c8326ba25365218c30e5f026ac
             telemetry.update();
 
 
-
-
-<<<<<<< HEAD
-
-
-
-
-=======
->>>>>>> a0bbf905b93dd7c8326ba25365218c30e5f026ac
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<<<<<<< HEAD
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=======
->>>>>>> a0bbf905b93dd7c8326ba25365218c30e5f026ac
