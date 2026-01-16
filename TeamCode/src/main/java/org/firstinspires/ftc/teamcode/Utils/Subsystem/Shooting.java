f4d4ae3bfa6b2f7ef659576bfd5e14b0185fe47b
infinitebirds1 <willcryst20@gmail.com>
2026-01-15 23:47:47 -0800

    added setpoints and tweaked code
    hopefully this doesnt decimate the robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/Subsystem/Shooting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/Subsystem/Shooting.java
@@ -29,7 +29,7 @@ public class Shooting extends SubsystemBase {
     public static double kP = 0.0;
     public static double kI = 0.0;
     public static double kD = 0.0;
-    public static double kF = 0.0
+    public static double kF = 0.0;
     public static double kS = 0.0;
     public static double kV = 0.0;
 
