ffab558a5f3e0358ed83d3692dfdb05d57522fc3
local.properties <shinyantsukamoto@gmail.com>
2026-01-16 16:19:47 -0800

    shooting @Config

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/Subsystem/Shooting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/Subsystem/Shooting.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.Utils.Subsystem;
 import androidx.annotation.NonNull;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.arcrobotics.ftclib.controller.PIDController;
@@ -15,7 +16,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.Range;
 
-
+@Config
 public class Shooting extends SubsystemBase {
 
 
