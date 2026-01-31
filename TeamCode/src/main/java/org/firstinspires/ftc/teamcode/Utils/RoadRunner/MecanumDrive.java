3a348bc8b511075ada687289fd68c151372f1569
wilcoxrobotics <you@example.com>
2026-01-30 18:18:06 -0800

    Roadrunner Tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/RoadRunner/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/RoadRunner/MecanumDrive.java
@@ -256,7 +256,7 @@ public final class MecanumDrive {
 
         voltageSensor = hardwareMap.voltageSensor.iterator().next();
 
-        localizer = new TwoDeadWheelLocalizer(hardwareMap, lazyImu.get(), PARAMS.inPerTick, pose);
+        localizer = new OTOSLocalizer(hardwareMap, pose);
 
         FlightRecorder.write("MECANUM_PARAMS", PARAMS);
     }
