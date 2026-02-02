aa2f8a63abbda3aba29d90b2e77653c9106de687
riteshseerapu <riteshrseerapu@gmail.com>
2026-02-02 14:55:02 -0800

    newteleop with command

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/feb2.java
@@ -0,0 +1,152 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.InstantAction;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.teamcode.Utils.Subsystem.*;
+
+@TeleOp
+public class feb2 extends LinearOpMode {
+    private Intake Intake;
+    private DriveTrain drive;
+    private OuttakeSubsystem outtake;
+
+
+    private boolean platformOn = false;
+    private boolean intakeOn = false;
+    private boolean outtakeOn = false;
+
+    private boolean rumble = false;
+    private static double MAX_TICKS = 2500;
+
+    private ElapsedTime rumbleTime = new ElapsedTime();
+
+    private boolean leftTriggerPressed;
+    private boolean rightTriggerPressed;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        drive = new DriveTrain(hardwareMap);
+        outtake = new OuttakeSubsystem(hardwareMap);
+
+        Gamepad currentGamepad2 = new Gamepad();
+        Gamepad previousGamepad2 = new Gamepad();
+
+        waitForStart();
+        if (isStopRequested()) return;
+
+        rumbleTime.reset();
+
+        while (opModeIsActive() && !isStopRequested()) {
+
+            previousGamepad2.copy(currentGamepad2);
+            currentGamepad2.copy(gamepad2);
+
+            double y = -gamepad1.left_stick_y;
+            double rx = gamepad1.left_stick_x * 1.1;
+            double x = gamepad1.right_stick_x;
+
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+
+            drive.frontLeftMotor.setPower((y + x + rx) / denominator);
+            drive.backLeftMotor.setPower((y - x + rx) / denominator);
+            drive.frontRightMotor.setPower((y - x - rx) / denominator);
+            drive.backRightMotor.setPower((y + x - rx) / denominator);
+
+            leftTriggerPressed = currentGamepad2.left_trigger > 0.7 && !(previousGamepad2.left_trigger > 0.7);
+
+            rightTriggerPressed = currentGamepad2.right_trigger > 0.7 && !(previousGamepad2.right_trigger > 0.7);
+
+
+            if (leftTriggerPressed) {
+                intakeOn = !intakeOn;
+            }
+
+            if (rightTriggerPressed) {
+                outtakeOn = !outtakeOn;
+            }
+
+            Intake.intakeMotor.setPower(intakeOn ? 0.7 : 0);
+            outtake.outtakeMotor.setPower(outtakeOn ? 0.8 : 0);
+
+            if (currentGamepad2.cross && !previousGamepad2.cross) {
+                platformOn = !platformOn;
+            }
+
+            if (platformOn) {
+                outtake.platformServo.setPosition(1);
+            } else {
+                outtake.platformServo.setPosition(0);
+            }
+
+            if (Math.abs(outtake.outtakeMotor.getVelocity() - outtake.outtakeMotor.getPower() * MAX_TICKS) < Shooting.outtakeTolerance
+                    && rumbleTime.seconds() > 1
+                    && outtake.outtakeMotor.getPower() > 0.3) {
+
+                rumble = true;
+                rumbleTime.reset();
+            }
+
+            if (rumble) {
+                rumble = false;
+                gamepad2.rumble(500);
+                gamepad2.setLedColor(29, 67, 107, 1000);
+            }
+
+            telemetry.addLine("Left Trigger: Intake @ 0.7");
+            telemetry.addLine("Right Trigger: Outtake @ 0.8");
+            telemetry.addLine("Platform: X");
+
+            telemetry.addData("Intake", intakeOn ? "ON" : "OFF");
+            telemetry.addData("Outtake", outtakeOn ? "ON" : "OFF");
+            telemetry.addData("Platform", platformOn ? "ON" : "OFF");
+
+            telemetry.addData("Outtake Power", outtake.outtakeMotor.getPower());
+            telemetry.addData("Outtake Velocity", outtake.outtakeMotor.getVelocity());
+
+            telemetry.update();
+        }
+        CommandScheduler.getInstance().schedule(
+                new SequentialCommandGroup(
+
+                )
+
+
+
+
+
+
+
+
+        );
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        CommandScheduler.getInstance().run();
+
+        Action shoot = new SequentialAction(
+
+                new InstantAction(() -> outtake.outtakeMotor.setPower(1)),
+                new ParallelAction(new InstantAction(()-> outtake.platformServo.setPosition(1))
+                )
+        );
+        Action intake = new SequentialAction(
+
+                new InstantAction(() -> Intake.intakeMotor.setPower(1))
+        );
+        Action hoodservoup = new SequentialAction(
+
+                new InstantAction(() -> outtake.platformServo.setPosition(1))
+        );
+    }
+}
